d9449a6000d2922ebec6b2ed8765c70497e64405
sainsh <kfsainsh@gmail.com>
2018-10-02 10:19:05 +0200

    one

---
+++ src/one/Main.java
@@ -0,0 +1,21 @@
+package one;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("enter the length from the center to a vertex: ");
+        double r = in.nextDouble();
+
+        double s = 2*r*Math.sin(Math.PI/5);
+
+        double area = (5*Math.pow(s,2))/(4*Math.tan(Math.PI/5));
+
+        System.out.println("The area of the pentagon is " + area);
+
+    }
+}
